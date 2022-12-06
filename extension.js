4446736ae05c2b0bd65f8a6fa7d84c879cad526e
Taylor Bell <taylorbell@gmail.com>
2022-12-05 21:02:33 -0700

    fix range so it really is the next one

--- extension.js
+++ extension.js
@@ -14,23 +14,31 @@ function activate(context) {
 		let imgTagEnd = '>';
 
 		// regex to find the next img tag
-		let imgTagRegex = new RegExp(imgTagStart + '.*' + imgTagEnd, 'g');
+		// let imgTagRegex = new RegExp(imgTagStart + '.*' + imgTagEnd, 'g');
+		let imgTagRegex = new RegExp(`(?<=${editor.document.getText(editor.selection)})${imgTagStart}.*${imgTagEnd}`, 'g');
+
 		
 		// delete the next imgTagRegex match
 		editor.edit(editBuilder => {
 			let cursorPosition = editor.selection.active;
-			let text = editor.document.getText();
+
+			// get the entire document text
+			let range = new vscode.Range(cursorPosition, editor.document.lineAt(editor.document.lineCount - 1).range.end);
+
+			let text = editor.document.getText(range);
+
 			let match = imgTagRegex.exec(text);
 			if (match) {
-				let startPos = editor.document.positionAt(match.index);
-				let endPos = editor.document.positionAt(match.index + match[0].length);
+				// calculate the start and end positions for the match
+				let startPos = editor.document.positionAt(editor.document.offsetAt(range.start) + match.index);
+				let endPos = editor.document.positionAt(editor.document.offsetAt(range.start) + match.index + match[0].length);
+
+				// delete the img tag
 				editBuilder.delete(new vscode.Range(startPos, endPos));
 			}
 		});
-
 	});
 
-	context.subscriptions.push(disposable);
 }
 
 
